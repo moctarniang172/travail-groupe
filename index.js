c197a538e208699689852b5d239461be01322020
moctar <niangmoctar172@gmail.com>
2026-02-08 15:56:13 +0000

    projet groupe taiwindcss

---
+++ index.js
@@ -0,0 +1,14 @@
+ tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+            bleu: '#a740ff',
+            rose: '#f42683',
+            fond3: '#f5f4ff',
+          }
+        }
+      }
+    }
+
+
