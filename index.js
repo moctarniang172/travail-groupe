8b068dffdb0d6370300fb3350e11a6e722cf86f0
moctar <niangmoctar172@gmail.com>
2026-02-12 04:59:32 +0000

    encore quelque modifications

--- index.js
+++ index.js
@@ -3,12 +3,18 @@
         extend: {
           colors: {
             clifford: '#da373d',
-            bleu: '#a740ff',
+            bleu: '#4954fb',
             rose: '#f42683',
             fond3: '#f5f4ff',
+            noire: '#2c2c2c',
+            footer: '#f9fafb',
+            jaune : '#ffd43b',
           }
         }
       }
     }
+
+
+
 
 
