224e518d4f14a2445d3a4e916b401c9944d48459
moctarniang172 <niangmoctar172@gmail.com>
2026-02-12 05:29:40 +0000

    Revert "Moctar"

--- index.js
+++ index.js
@@ -3,18 +3,12 @@
         extend: {
           colors: {
             clifford: '#da373d',
-            bleu: '#4954fb',
+            bleu: '#a740ff',
             rose: '#f42683',
             fond3: '#f5f4ff',
-            noire: '#2c2c2c',
-            footer: '#f9fafb',
-            jaune : '#ffd43b',
           }
         }
       }
     }
-
-
-
 
 
